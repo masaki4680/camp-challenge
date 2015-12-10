08d74f93fbbb53a05a2e3aedc88b18de80f98e8d
Masaki Nakamura <masaki46803980@gmail.com>
2015-12-10 12:56:39 +0900

    改定です。

--- 03.kadai7.php
+++ 03.kadai7.php
@@ -5,29 +5,29 @@
 
 <?php
 // 課題7:引き数、戻り値はなしの関数を用意。
-//初期値3のグローバルな値を2倍していく、ローカルな値はstaticとして
+//初期値3のグローバルな値を2倍していく＋ローカルな値はstaticとして
 //その関数が何回実行されたのかを保持していくような関数である。
 //この関数を20回呼び出す
 
 
 
 //グローバル値
-$initial_number=3;
+$number=3;
 
 function test(){
-	global $initilal_number;
+	
+    global $number;
+    
+    $number *= 2;
+    
+    static $count =0;
+    
+    $count++;
+    
+    echo $count."回";
+   
 
-	$local_number=$initilal_number*2;
-	//何回実行したのかを保持するやり方がわからない。
-	static $local_number;
-
-	static $count = 0;
-
-	 $count++;
-
-	 echo $count."回";
-
-
+echo $number."<br>";
 
 }
 
