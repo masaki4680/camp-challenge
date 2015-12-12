01a64cc880d9e6703e9b7eb1094ecdb9ec131a95
Masaki Nakamura <masaki46803980@gmail.com>
2015-12-13 00:35:02 +0900

    12月12日　課題　訂正も含む

---
+++ 05kadai3.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <meta charset="utf-8">
+</html>
+<?PHP
+//３．クッキーに現在時刻を記録し、次にアクセスした際に、前回記録した日時を表示してください。
+
+
+
+if (!isset($_COOKIE["Lastlogintime"])){//初回アクセス時
+    
+    $access_time=date("Y年m月d日　H時i分s秒");
+    
+    setcookie("Lastlogintime",$access_time);
+    
+    echo "初回のアクセスです。";
+    
+}else{//2回目以降のログイン
+   
+    if(!isset($_COOKIE["Nexttime"])){//nexttimeがないとき
+        
+        $last_login_time = $_COOKIE["Lastlogintime"];
+    
+    echo "2回目のアクセス時は".$last_login_time;
+        
+        $number=2;
+    
+    }else{//nexttimeがあるとき
+        
+        $number=$_COOKIE["number"];
+        
+        $next_time = $_COOKIE["Nexttime"];
+        
+        echo $number."回目のアクセス時は".$next_time;
+        
+    
+    }
+    
+    $number++;
+    
+    setcookie("number",$number);
+    
+    $login_time = date("Y年m月d日　H時i分s秒");
+    
+    setcookie("Nexttime",$login_time);
+        
+    
+        
+    }
+
+?>
---
+++ 05kadai4.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <meta charset="utf-8">
+</html>
+<?PHP
+//4.セッションに現在時刻を記録し、次にアクセスした際に、前回記録した日時を表示してください。
+session_start();
+
+if(!isset($_SESSION["login"])){
+    
+    $_SESSION["login"] = date("Y-m-d H-i-s");
+    
+    
+    echo "初めてのアクセスです。";
+    
+}else{
+    
+    if(!isset($_SESSION["second"])){
+        
+        echo "2回目のアクセス日時は".$_SESSION["login"];
+        
+        $_SESSION["visited"] = 2;
+        
+    }else{
+        
+        echo $_SESSION["visited"]."回目のアクセス日時は".$_SESSION["second"];
+        
+    }
+    
+    $_SESSION["second"] = date("Y-m-d H-i-s");
+          
+    $_SESSION["visited"]++;
+        
+}
+?>
