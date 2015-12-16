38df300127336d44fd53df769ca7704be32cf58a
Masaki Nakamura <masaki46803980@gmail.com>
2015-12-16 17:23:48 +0900

    変数に対するダブルくおてーしょんの扱い
    添削再提出

--- 04kadai7.php
+++ 04kadai7.php
@@ -7,6 +7,7 @@
 //    結果を表示してください。
 //    「きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます」
 $mozi="きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
-$amozi = str_replace('I','い',"$mozi");
-echo str_replace('U','う',"$amozi");
+//変数が文字とわかっているんであれば、囲う必要ない
+$amozi = str_replace('I','い',$mozi);
+echo str_replace('U','う',$amozi);
 ?>
