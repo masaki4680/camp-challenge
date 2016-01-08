8b70f5f90a2f7da591890435cb5f1c4e2fd6ca93
Masaki Nakamura <masaki46803980@gmail.com>
2016-01-08 18:02:31 +0900

    ・$flag == falseが入ってない
    ・bimdValueが条件分岐されていない
    
    ・$_GET[]→$_POST[]
    に変更

---
+++ search.php
@@ -0,0 +1,40 @@
+<?php require_once './defineUtil.php'; ?>
+<?php require_once './scriptUtil.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+<meta charset="UTF-8">
+      <title>ユーザー情報検索画面</title>
+</head>
+  <body>
+<!--   GET→POSTに変更-->
+    <form action="<?php echo SEARCH_RESULT ?>" method="POST">
+        
+        名前:
+        <br>
+        <input type="text" name="name">
+        <br><br>
+
+        生年:
+        <br>
+        <select name="year">
+            <option value="">----</option>
+            <?php
+            for($i=1950; $i<=2010; $i++){ ?>
+              <option value="<?php echo $i;?>"><?php echo $i;?></option>
+            <?php } ?>
+        </select>年生まれ
+        <br><br>
+
+        種別:
+        <br>
+        <?php
+        for($i = 1; $i<=3; $i++){ ?>
+        <input type="radio" name="type" value="<?php echo $i; ?>"><?php echo ex_typenum($i);?><br>
+        <?php } ?>
+        <br>
+        <input type="submit" name="btnSubmit" value="検索">
+      </form>
+  </body>
+</html>
---
+++ search_result.php
@@ -0,0 +1,44 @@
+<?php 
+require_once './defineUtil.php';
+require_once './scriptUtil.php';
+require_once './dbaccesUtil.php';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+      <title>検索結果画面</title>
+</head>
+    <body>
+        <h1>検索結果</h1>
+        <table border=1>
+            <tr>
+                <th>名前</th>
+                <th>生年</th>
+                <th>種別</th>
+                <th>登録日時</th>
+            </tr>
+        <?php
+        $result = null;
+//(GETをPOSTに変更) 
+        if(empty($_POST['name']) && empty($_POST['year']) && empty($_POST['type'])){
+            
+            $result = serch_all_profiles();
+            
+        }else{
+ $result = serch_profiles($_POST['name'],$_POST['year'],$_POST['type']);
+        }
+        foreach($result as $value){
+        ?>
+            <tr>
+                <td><a href="<?php echo RESULT_DETAIL ?>?id=<?php echo $value['userID']?>"><?php echo $value['name']; ?></a></td>
+                <td><?php echo $value['birthday']; ?></td>
+                <td><?php echo ex_typenum($value['type']); ?></td>
+                <td><?php echo date('Y年n月j日　G時i分s秒', strtotime($value['newDate']));; ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+        </table>
+</body>
+</html>
